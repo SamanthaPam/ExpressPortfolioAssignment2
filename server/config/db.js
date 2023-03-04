e32dafc337c20abec963c8328b0196e2f299314f
SamanthaPam <122388897+SamanthaPam@users.noreply.github.com>
2023-03-04 18:09:41 -0500

    password DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports =
 {
-    "URI":"mongodb+srv://Samantha16:bvZipB4AZBNAuk3Z@cluster0.wzrzefk.mongodb.net/business_contacts_data"
+    "URI":"mongodb+srv://Samantha16:9ccuFYVePhHGxkKO@cluster0.wzrzefk.mongodb.net/business_contacts_data"
 }
